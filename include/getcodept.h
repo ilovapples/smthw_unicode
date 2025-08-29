e361fa03ddbaa50d04ce86ee2e9312a47ad4febc
Asher Grossman <ashergrossman185@gmail.com>
2025-08-29 10:40:12 -0500

    should work now

--- include/getcodept.h
+++ include/getcodept.h
@@ -1,5 +1,18 @@
 #include "aplcore/types.h"
 
+typedef struct {
+	union {
+		u32 i;
+		u8 b[4];
+	} val;
+	enum {
+		UCP_ASCII_TYPE,
+		UCP_2BYTE_TYPE,
+		UCP_3BYTE_TYPE,
+		UCP_4BYTE_TYPE
+	} type;
+} UCP;
+
 void get_bytes_from_codepoint_str(
 	const char *cp_str,
 	u8 *out_bytes,
